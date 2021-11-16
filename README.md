344b8f065aa6f69dbebb93832ff847dce68125c8
Billy Bunn <billy@billybunn.io>
2021-11-15 16:14:45 -0800

    creates cloudfront distribution of s3 bucket contents

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 # 🚧 Currently under construction 🚧
 
-##  Goals
+## Goals
+
 - At all costs, "infrastructure as code" (I'm going with [AWS Cloud Development Kit (CDK)](https://aws.amazon.com/cdk/))
 - Document and present what I learn in the process of building it
 - A place for showcasing my work
 - A place for writing learnings and musings
-
 
 # Welcome to your CDK TypeScript project!
 
@@ -17,9 +17,9 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
--- index.ts
+++ index.ts
@@ -1,12 +1,50 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 
 class WebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    /**
+     * S3 bucket with website contents
+     * https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-s3.Bucket.html
+     * autoDeleteObjects and removalPolicy set so entire bucket and contents are removed on stack deletion
+     */
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      autoDeleteObjects: true,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "404.html",
+    });
+
+    /**
+     * S3 bucket deployment
+     * https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-s3-deployment.BucketDeployment.html
+     * Specifies source folder for bucket files
+     */
+    new s3Deployment.BucketDeployment(this, "WebsiteBucketDeployment", {
+      destinationBucket: websiteBucket,
+      sources: [s3Deployment.Source.asset("./website-content")],
+    });
+
+    /**
+     * CloudFront distribution of website bucket
+     * https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.Distribution.html
+     */
+    new cloudfront.Distribution(this, 'WebsiteBucketDistribution', {
+      defaultBehavior: {
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+      }
+    })
+
   }
 }
 
 const app = new cdk.App();
-new WebsiteStack(app, 'WebsiteStack');
+new WebsiteStack(app, "WebsiteStack");
