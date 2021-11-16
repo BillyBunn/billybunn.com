ac5fe52dff4d784e4cd24ade91f3f213a3a0c48f
Billy Bunn <billy@billybunn.io>
2021-11-16 10:22:05 -0800

    updates cloudfront distribution of bucket

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
-import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 
 class WebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -12,13 +12,42 @@ class WebsiteStack extends cdk.Stack {
      * S3 bucket with website contents
      * https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-s3.Bucket.html
      * autoDeleteObjects and removalPolicy set so entire bucket and contents are removed on stack deletion
+     * static web hosting is not enabled here with websiteIndexDocument to hide the bucket's (non-CloudFront) endpoint
+     * Without a websiteIndexDocument, the bucket is configured as a S3 origin, not an HTTP origin
+     * meaning an OAI and bucket policy will be created for the CloudFront distribution
+     * https://github.com/aws/aws-cdk/issues/14019
      */
     const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
       autoDeleteObjects: true,
-      publicReadAccess: true,
+      publicReadAccess: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      websiteIndexDocument: "index.html",
-      websiteErrorDocument: "404.html",
+    });
+    
+    /**
+     * CloudFront distribution of website bucket
+     * https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.Distribution.html
+     * PRICE_CLASS_100: USA, Canada, Europe, & Israel
+     * https://aws.amazon.com/cloudfront/pricing/
+     */
+    const websiteBucketDistribution = new cloudfront.Distribution(this, "WebsiteBucketDistribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: "index.html",
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 404,
+          responsePagePath: "/404.html",
+        },
+        {
+          httpStatus: 404,
+          responseHttpStatus: 404,
+          responsePagePath: "/404.html",
+        },
+      ],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100
     });
 
     /**
@@ -28,21 +57,10 @@ class WebsiteStack extends cdk.Stack {
      */
     new s3Deployment.BucketDeployment(this, "WebsiteBucketDeployment", {
       destinationBucket: websiteBucket,
+      distribution: websiteBucketDistribution,
+      distributionPaths: ['/*'],
       sources: [s3Deployment.Source.asset("./website-content")],
     });
-
-    /**
-     * CloudFront distribution of website bucket
-     * https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.Distribution.html
-     */
-    new cloudfront.Distribution(this, 'WebsiteBucketDistribution', {
-      defaultBehavior: {
-        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
-        origin: new origins.S3Origin(websiteBucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
-      }
-    })
-
   }
 }
 
